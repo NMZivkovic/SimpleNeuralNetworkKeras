87dda464b7ddb721015a137af01d23192aeca9c5
NMZivkovic <zivkovic.nikola87@gmail.com>
2018-02-12 09:11:46 +0100

    Documentation changes

--- ann_keras.py
+++ ann_keras.py
@@ -2,7 +2,8 @@
 Created on Mon Feb 10 2018
 
 @author: Nikola Zivkovic
-This repository contains examples used in blogpost - 
+This repository contains examples used in blogpost - This repository contains examples used in blogpost
+ - http://rubikscode.net/2018/02/12/implementing-simple-neural-network-using-keras-with-python-example/
 """
 
 # Importing libraries
